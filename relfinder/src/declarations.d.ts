8c99f51ae407c38fd2a033b177f951d37f4a433c
Kody Moodley <kody.moodley@gmail.com>
2026-04-09 12:31:55 +0200

    Enables import of TTL files as raw strings

--- relfinder/src/declarations.d.ts
+++ relfinder/src/declarations.d.ts
@@ -3,3 +3,9 @@ declare module 'cytoscape-d3-force' {
   const d3Force: (cy: Core) => void
   export default d3Force
 }
+
+// Allow importing any .ttl file as a raw string via Vite's `?raw` query.
+declare module '*.ttl?raw' {
+  const content: string
+  export default content
+}
